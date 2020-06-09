9b75df8c826438ff4aefa7c5b83088340e44fbdd
sangboi2 <phtha12@gmail.com>
2020-06-09 18:37:21 +0200

    Improved

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,18 @@
+$(function () {
 
+    $(".form-control").on('focus', function () {
+
+        $(this).parents(".form-group").addClass('focused');
+
+    });
+
+    $(".form-control").on('blur', function () {
+
+        $(this).parents(".form-group").removeClass('focused');
+
+    });
+
+});
 // Run wow.js
 new WOW().init();
 // Global
